53bc671970a9daa1cefe1b8c93a9d4428c2a0d50
nnejiobioma <nnejiobioma@gmail.com>
2023-01-24 06:44:48 +0000

    made changes to main.c and main.h file moved the files to test folder

--- test/main.h
+++ test/main.h
@@ -22,8 +22,8 @@
  *  * struct fmt - Struct op
  *   *
  *    * @fmt: The format.
- *     * @fn: The function associated
-*/
+ *     * @fn: The function associated.
+ *      */
 struct fmt
 {
 		char fmt;
@@ -36,7 +36,7 @@ struct fmt
  *   *
  *    * @fmt: The format.
  *     * @fm_t: The function associated.
-*/
+ *      */
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
